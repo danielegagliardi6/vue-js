29d1f1ad77031fb6103b157b5717a28740a0dcc6
Daniele Gagliardi <daniele.gagliardi@pwc.com>
2020-11-23 18:27:40 +0100

    fix env

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
+require('dotenv').config();
 
 export default axios.create({
-  baseURL: "http://exp.localhost/api",
-  headers: {
-    "Content-type": "application/json"
-  }
+    baseURL: process.env.VUE_APP_BACKEND_URL || "http://localhost:3000/api",
+    headers: {
+        "Content-type": "application/json"
+    }
 });
